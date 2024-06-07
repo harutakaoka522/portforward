722b13f3eb07920cdf8d5cb034746e66cf63c9d8
harutakaoka522 <48231451+harutakaoka522@users.noreply.github.com>
2024-06-07 13:49:07 +0900

    Create portfowarding-rds-instance.sh

---
+++ portfowarding-rds-instance.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# 利用可能なAWSプロファイルのリストを表示
+echo "利用可能なAWSプロファイル:"
+aws configure list-profiles
+
+# AWSプロファイル名の入力を求める
+read -p "利用するAWSプロファイル名を入力してください: " AWS_PROFILE
+
+# AWSリージョンを指定
+AWS_REGION="ap-northeast-1"
+
+# EC2インスタンス名のリストを取得
+echo "利用可能なEC2インスタンス (起動中):"
+aws ec2 describe-instances --profile ${AWS_PROFILE} --region ${AWS_REGION} --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' --output text
+
+# ユーザーにEC2インスタンス名を選択させる
+read -p "利用するEC2インスタンス名を入力してください: " INSTANCE_NAME
+
+# RDSインスタンス名のリストを取得
+echo "利用可能なRDSインスタンス:"
+aws rds describe-db-instances --profile ${AWS_PROFILE} --region ${AWS_REGION} --query 'DBInstances[*].DBInstanceIdentifier' --output text
+
+# ユーザーにRDSインスタンス名を選択させる
+read -p "利用するRDSインスタンス名を入力してください: " RDS_INSTANCE_IDENTIFIER
+
+# インスタンスIDを取得
+INSTANCE_ID=$(aws ec2 describe-instances --profile ${AWS_PROFILE} --region ${AWS_REGION} --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query "Reservations[*].Instances[*].InstanceId" --output text)
+
+# インスタンスIDが取得できなかった場合の処理
+if [ -z "$INSTANCE_ID" ]; then
+  echo "インスタンスIDが取得できませんでした。インスタンス名を確認してください。"
+  exit 1
+fi
+
+# RDSエンドポイントを取得
+RDS_ENDPOINT=$(aws rds describe-db-instances --profile ${AWS_PROFILE} --region ${AWS_REGION} --db-instance-identifier ${RDS_INSTANCE_IDENTIFIER} --query "DBInstances[0].Endpoint.Address" --output text)
+
+# RDSエンドポイントが取得できなかった場合の処理
+if [ -z "$RDS_ENDPOINT" ]; then
+  echo "RDSエンドポイントが取得できませんでした。RDSインスタンス名を確認してください。"
+  exit 1
+fi
+
+# ポートフォワーディングセッションの開始
+aws ssm start-session --profile ${AWS_PROFILE} --region ${AWS_REGION} \
+    --target ${INSTANCE_ID} \
+    --document-name AWS-StartPortForwardingSessionToRemoteHost \
+    --parameters "{\"host\":[\"${RDS_ENDPOINT}\"],\"portNumber\":[\"3306\"], \"localPortNumber\":[\"3306\"]}"
